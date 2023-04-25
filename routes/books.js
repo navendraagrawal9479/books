827b4d7ac09ed1d1c9bc30d5b651d86833e86be0
navendraagrawal9479 <75437871+navendraagrawal9479@users.noreply.github.com>
2023-04-25 16:40:32 +0530

    Add files via upload

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addBook, getAllBooks, getIndividualBook, recommendBook, searchBook } from '../controllers/books.js';
+import { addBook, getAllBooks, getIndividualBook, searchBook } from '../controllers/books.js';
 import singleUpload from '../middlewares/multer.js';
 
 const router = express.Router();
@@ -7,7 +7,6 @@ const router = express.Router();
 router.get('/', getAllBooks);
 router.get('/:id', getIndividualBook);
 router.get('/search-book/:q', searchBook);
-router.get('/recommend-book/:q', recommendBook);
 
 router.post('/', singleUpload, addBook)
 
